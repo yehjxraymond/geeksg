c1d7a5dbd955cafb63645b161addfb275f2e19be
Raymond Yeh <yehjxraymond@gmail.com>
2018-12-25 11:14:23 +0800

    Updated about

--- components/registry.js
+++ components/registry.js
@@ -1,5 +1,15 @@
-export { meta as Test } from "../pages/blog/test.mdx";
-export { meta as Post2 } from "../pages/blog/post2.mdx";
 export {
   meta as EOS
 } from "../pages/blog/getting-started-with-eos-on-cleos-without-running-a-full-node.mdx";
+export {
+  meta as DATA_MINING_POKEMON
+} from "../pages/blog/data-mining-pokemon-spawns-in-singapore.mdx";
+export {
+  meta as TOP_10_RAREST_GEN_2_POKEMON
+} from "../pages/blog/top-10-rarest-gen-2-pokemon-singapore.mdx";
+export {
+  meta as FINDING_DRATINI_NEST_DB_SCAN
+} from "../pages/blog/finding-dratini-nest-dbscan.mdx";
+export {
+  meta as STEP_BY_STEP_POKEMON_CLUSTERING
+} from "../pages/blog/step-by-step-pokemon-clustering.mdx";
