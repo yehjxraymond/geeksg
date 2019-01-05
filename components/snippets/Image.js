065d1d259ac4e768b4fde45e6174f35da023b362
Raymond Yeh <yehjxraymond@gmail.com>
2019-01-05 13:48:36 +0800

    Added Image and Video snippets

---
+++ components/snippets/Image.js
@@ -0,0 +1,60 @@
+import PropTypes from "prop-types";
+
+const Caption = caption => <div className="h6 my-2">{caption}</div>;
+
+const Citation = (cite, citeSrc) => {
+  if (!cite && !citeSrc) return null;
+  return citeSrc ? (
+    <div style={{ fontSize: "0.8rem" }}>
+      <a className="text-light" href={citeSrc}>
+        Image from {cite || citeSrc}
+      </a>
+    </div>
+  ) : (
+    <div className="text-light">Image from {cite}</div>
+  );
+};
+
+export const Image = ({
+  src,
+  width = 100,
+  caption,
+  cite,
+  citeSrc,
+  href,
+  alt
+}) => (
+  <div className="text-center m-4">
+    <div>
+      {href ? (
+        <a href={href}>
+          <img
+            src={src}
+            className={`w-100 w-xl-${width} w-lg-${width}`}
+            alt={alt || caption}
+          />
+        </a>
+      ) : (
+        <img
+          src={src}
+          className={`w-100 w-xl-${width} w-lg-${width}`}
+          alt={alt || caption}
+        />
+      )}
+    </div>
+    {Citation(cite, citeSrc)}
+    {Caption(caption)}
+  </div>
+);
+
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  href: PropTypes.string,
+  width: PropTypes.string,
+  caption: PropTypes.string,
+  cite: PropTypes.string,
+  citeSrc: PropTypes.string
+};
+
+export default Image;
---
+++ components/snippets/Video.js
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+export const Youtube = ({ url }) => (
+  <div className="embed-responsive embed-responsive-16by9 my-5">
+    <iframe
+      className="embed-responsive-item"
+      src={`https://www.youtube-nocookie.com/embed/${url}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    />
+  </div>
+);
+
+Youtube.propTypes = {
+  url: PropTypes.string
+};
+
+export default Youtube;
