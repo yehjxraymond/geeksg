20ba1b06dd398377fa83b31bc1307b78337d14d2
Raymond Yeh <yehjxraymond@gmail.com>
2019-12-20 00:37:18 +0800

    feat: e2e encryption qv

--- components/registry.js
+++ components/registry.js
@@ -1,43 +1,16 @@
-export {
-  meta as QUADRATIC_VOTING_GROUP_CONSENSUS
-} from "../pages/blog/quadratic-voting-group-consensus.mdx";
-export {
-  meta as COMPARISON_OPTIMISATION_TECHNIQUES
-} from "../pages/blog/comparison-portfolio-optimization-technique-monte-carlo-slsqp-bayesian.mdx";
+export { meta as E2E_ENCRYPTION_QUADRATIC_VOTING } from "../pages/blog/end-to-end-encrypted-quadratic-voting-app.mdx";
+export { meta as QUADRATIC_VOTING_GROUP_CONSENSUS } from "../pages/blog/quadratic-voting-group-consensus.mdx";
+export { meta as COMPARISON_OPTIMISATION_TECHNIQUES } from "../pages/blog/comparison-portfolio-optimization-technique-monte-carlo-slsqp-bayesian.mdx";
 export { meta as DEAR_RECRUITERS } from "../pages/blog/dear-recruiters";
-export {
-  meta as REFLECTING_TWO_YEARS_INVESTMENT
-} from "../pages/blog/reflecting-two-years-investing.mdx";
-export {
-  meta as HFT_BOT_PROCESSING_10_MILLION_MONTH
-} from "../pages/blog/learn-from-building-hft-bot-processing-10-million-month.mdx";
-export {
-  meta as SNAPSHOT_ETHEREUM_ACCOUNTS_BALANCE
-} from "../pages/blog/snapshot-ethereum-accounts-balance-under-minute.mdx";
-export {
-  meta as EOS
-} from "../pages/blog/getting-started-with-eos-on-cleos-without-running-a-full-node.mdx";
-export {
-  meta as DATA_MINING_POKEMON
-} from "../pages/blog/data-mining-pokemon-spawns-in-singapore.mdx";
-export {
-  meta as TOP_10_RAREST_GEN_2_POKEMON
-} from "../pages/blog/top-10-rarest-gen-2-pokemon-singapore.mdx";
-export {
-  meta as FINDING_DRATINI_NEST_DB_SCAN
-} from "../pages/blog/finding-dratini-nest-dbscan.mdx";
-export {
-  meta as STEP_BY_STEP_POKEMON_CLUSTERING
-} from "../pages/blog/step-by-step-pokemon-clustering.mdx";
-export {
-  meta as USABILITY_MIDDLEGROUND_AUTHORITATIVE_FALLBACK_RELAY
-} from "../pages/blog/usability-middleground-blockchain-solutions-authoritative-fallback-relays.mdx";
-export {
-  meta as LOAD_TEST_TREBUCHET_FRAMEWORK
-} from "../pages/blog/load-test-private-ethereum-network-trebuchet-framework.mdx";
-export {
-  meta as THIRD_PARTY_SCAMS_MONEY_LAUNDERING
-} from "../pages/blog/third-party-scams-money-laundering-journey-dark-side.mdx";
-export {
-  meta as SMART_CONTRACT_FOR_HIRING
-} from "../pages/blog/smart-contract-for-hiring.mdx";
+export { meta as REFLECTING_TWO_YEARS_INVESTMENT } from "../pages/blog/reflecting-two-years-investing.mdx";
+export { meta as HFT_BOT_PROCESSING_10_MILLION_MONTH } from "../pages/blog/learn-from-building-hft-bot-processing-10-million-month.mdx";
+export { meta as SNAPSHOT_ETHEREUM_ACCOUNTS_BALANCE } from "../pages/blog/snapshot-ethereum-accounts-balance-under-minute.mdx";
+export { meta as EOS } from "../pages/blog/getting-started-with-eos-on-cleos-without-running-a-full-node.mdx";
+export { meta as DATA_MINING_POKEMON } from "../pages/blog/data-mining-pokemon-spawns-in-singapore.mdx";
+export { meta as TOP_10_RAREST_GEN_2_POKEMON } from "../pages/blog/top-10-rarest-gen-2-pokemon-singapore.mdx";
+export { meta as FINDING_DRATINI_NEST_DB_SCAN } from "../pages/blog/finding-dratini-nest-dbscan.mdx";
+export { meta as STEP_BY_STEP_POKEMON_CLUSTERING } from "../pages/blog/step-by-step-pokemon-clustering.mdx";
+export { meta as USABILITY_MIDDLEGROUND_AUTHORITATIVE_FALLBACK_RELAY } from "../pages/blog/usability-middleground-blockchain-solutions-authoritative-fallback-relays.mdx";
+export { meta as LOAD_TEST_TREBUCHET_FRAMEWORK } from "../pages/blog/load-test-private-ethereum-network-trebuchet-framework.mdx";
+export { meta as THIRD_PARTY_SCAMS_MONEY_LAUNDERING } from "../pages/blog/third-party-scams-money-laundering-journey-dark-side.mdx";
+export { meta as SMART_CONTRACT_FOR_HIRING } from "../pages/blog/smart-contract-for-hiring.mdx";
